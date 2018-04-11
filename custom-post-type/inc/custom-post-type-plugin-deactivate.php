802a60d266cafead5d5bac7597667b2f283712eb
Dimitris Pantazatos <dim.pantazatos@gmail.com>
2018-04-11 16:49:33 +0300

    Update custom-post-type-plugin-deactivate.php

--- custom-post-type/inc/custom-post-type-plugin-deactivate.php
+++ custom-post-type/inc/custom-post-type-plugin-deactivate.php
@@ -1,1 +1,12 @@
+<?php
+/**
+* @package CustomPostType
+*/
 
+class CustomPostTypePluginDeactivate
+{
+  public static function deactivate() {
+    // flush rewrite rules
+    flush_rewrite_rules();
+  }
+}
