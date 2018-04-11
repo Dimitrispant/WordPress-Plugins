738f0ee199ddfeb6dee901086dea203b6c8ca17b
Dimitris Pantazatos <dim.pantazatos@gmail.com>
2018-04-11 16:49:07 +0300

    Update custom-post-type-plugin-activate.php

--- custom-post-type/inc/custom-post-type-plugin-activate.php
+++ custom-post-type/inc/custom-post-type-plugin-activate.php
@@ -1,1 +1,13 @@
+<?php
+/**
+* @package CustomPostType
+*/
+
+class CustomPostTypePluginActivate
+{
+  public static function activate() {
+    // flush rewrite rules
+    flush_rewrite_rules();
+  }
+}
 
