c58db12fd2320e192d54a286cf57c7f5f0713958
Dimitris Pantazatos <dim.pantazatos@gmail.com>
2018-04-11 15:09:20 +0300

    Create custom-post-type.php

---
+++ custom-post-type/custom-post-type.php
@@ -0,0 +1,14 @@
+<?php
+/**
+* @package CustomPostType
+*/
+/*
+Plugin Name: Custom Post Type
+Plugin URI: https://github.com/Dimitrispant/WordPress-Plugins
+Description: A simple custom post type plugin
+Version: 1.0
+Author: Dimitris Pantazatos
+Author URI: https://dpant.xyz
+License: GPLv2 or later
+Text Domain: custom-post-type
+*/
